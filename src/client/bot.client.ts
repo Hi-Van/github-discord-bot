b8e53ebf441c87ef837416d5aa806273a2af420b
Hi-Van <sirisoukvanouphon@gmail.com>
2024-05-04 05:36:48 -0500

    add thread ability

--- src/client/bot.client.ts
+++ src/client/bot.client.ts
@@ -1,7 +1,8 @@
-import { Client, Guild } from "discord.js";
+import { ChannelType, Client, Guild, Message } from "discord.js";
 import { deployCommands } from "../commands/deploy-commands";
 import { commands } from "../commands/index";
 import { config } from "../config/discord.config";
+import * as crypto from "crypto";
 
 class Discord {
   private client: Client;
@@ -12,8 +13,10 @@ class Discord {
     });
 
     this.client.once("ready", this.readyHandler);
+
     this.client.on("guildAvailable", this.guildCreateHandler);
     this.client.on("interactionCreate", this.interactionCreateHandler);
+    this.client.on("messageCreate", this.messsageCreateHandler);
 
     this.client.login(config.DISCORD_TOKEN);
   }
@@ -34,6 +37,47 @@ class Discord {
     const { commandName } = interaction;
     if (commands[commandName as keyof typeof commands]) {
       commands[commandName as keyof typeof commands].execute(interaction);
+    }
+  };
+
+  private messsageCreateHandler = async (message: Message) => {
+    if (message.author.bot || !message.guild) {
+      return;
+    }
+
+    switch (message.channel.type) {
+      case ChannelType.GuildText: {
+        const mentionsBot = !message.mentions.has(this.client.user!, {
+          ignoreEveryone: true,
+        });
+        if (mentionsBot) {
+          return;
+        }
+
+        const thread = await message.startThread({
+          name: crypto.randomUUID().toString(),
+          autoArchiveDuration: 60,
+          rateLimitPerUser: 3,
+          reason: `Conversation between ${message.author.tag} and ${this.client.user?.tag}`,
+        });
+
+        thread.send("message detected!");
+        break;
+      }
+
+      case ChannelType.PublicThread:
+      case ChannelType.PrivateThread: {
+        const thread = message.channel;
+
+        thread.send("Message detected!");
+        break;
+      }
+
+      default: {
+        message.channel.send(
+          "I'm sorry, I can only reply to text channels and threads."
+        );
+      }
     }
   };
 }
